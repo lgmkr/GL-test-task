8ae206796832d7536dd5292186fa4925501302be
Oleh Makarov <gstancox@gmail.com>
2020-04-10 19:00:08 +0300

    chore: clean up

--- test/server/server.spec.ts
+++ test/server/server.spec.ts
@@ -1,7 +1,7 @@
 import { Server } from "grpc";
 import { startServer, stopServer } from "../../src/server/server";
 import { GraphDispatcherService } from "../../src/proto/generated";
-import { GraphDispatcherHandler } from "../../src/server/handlers/graphDispatcherHandler";
+import { GraphHandler } from "../../src/server/handlers/graphHandler";
 
 const addServiceSpy = jest.spyOn(Server.prototype, "addService");
 const bindAsyncSpy = jest.spyOn(Server.prototype, "bindAsync");
@@ -22,7 +22,7 @@ describe("startServer", () => {
     expect(addServiceSpy).toBeCalledTimes(1);
     expect(addServiceSpy).toBeCalledWith(
       GraphDispatcherService,
-      new GraphDispatcherHandler()
+      new GraphHandler()
     );
 
     expect(bindAsyncSpy).toBeCalledTimes(1);
